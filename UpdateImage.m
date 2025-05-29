8f1342a5f545a58eef46f10f75fe5191e599d482
Kevin Tang <kevintang6142@gmail.com>
2025-05-29 15:56:22 -0500

    Implement multi-spectra mode

--- UpdateImage.m
+++ UpdateImage.m
@@ -2,6 +2,9 @@ classdef UpdateImage
     methods (Static)
         function updateImage(app)
             matrices = getappdata(0, 'myData');
+            if isempty(matrices)
+                return;
+            end
             Image = im2double(matrices.Images);
             diff = abs(round(app.BandSlider.Value)-matrices.Wavelengths);
             index = diff==min(diff);
@@ -46,33 +49,38 @@ classdef UpdateImage
             end
 
             imshow(I, [], 'Parent', app.image_axes, 'Colormap', cmap);
+            axis(app.image_axes, 'image')
             colorbar(app.image_axes, "Color", [1 1 1])
         end
         function updateRGB(app)
             matrices = getappdata(0, 'myData');
-                    Image = im2double(matrices.Images); 
+            if isempty(matrices)
+                return;
+            end
+            Image = im2double(matrices.Images); 
 
-                    diff = abs(round(app.Band1Slider.Value)-matrices.Wavelengths);
-                    band1 = diff==min(diff);
-                    diff = abs(round(app.Band2Slider.Value)-matrices.Wavelengths);
-                    band2 = diff==min(diff);
-                    diff = abs(round(app.Band3Slider.Value)-matrices.Wavelengths);
-                    band3 = diff==min(diff);
-                    
-                    R = Image(:, :, band1);
-                    G = Image(:, :, band2);
-                    B = Image(:, :, band3);
-                
-                    R = mat2gray(R);
-                    G = mat2gray(G);
-                    B = mat2gray(B);
-                
-                    RGB = cat(3, R, G, B);
-                    
-                    axes(app.image_axes);
-                    app.ColormapSelectionButtonGroup.Visible = "off";
-                    colorbar(app.image_axes, 'off')
-                    imshow(RGB,[],'Parent', app.image_axes);
+            diff = abs(round(app.Band1Slider.Value)-matrices.Wavelengths);
+            band1 = diff==min(diff);
+            diff = abs(round(app.Band2Slider.Value)-matrices.Wavelengths);
+            band2 = diff==min(diff);
+            diff = abs(round(app.Band3Slider.Value)-matrices.Wavelengths);
+            band3 = diff==min(diff);
+            
+            R = Image(:, :, band1);
+            G = Image(:, :, band2);
+            B = Image(:, :, band3);
+        
+            R = mat2gray(R);
+            G = mat2gray(G);
+            B = mat2gray(B);
+        
+            RGB = cat(3, R, G, B);
+            
+            axes(app.image_axes);
+            app.ColormapSelectionButtonGroup.Visible = "off";
+            colorbar(app.image_axes, 'off')
+            imshow(RGB,[],'Parent', app.image_axes);
+            axis(app.image_axes, 'image')
         end
     end
 end
